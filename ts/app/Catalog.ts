9d2f10d90e3705203714b62350b2d1aedc8e47d7
Scarlet <smichelle05a@gmail.com>
2021-09-25 11:02:18 -0300

    Add ProductList Interface and class Catalog

---
+++ ts/app/Catalog.ts
@@ -0,0 +1,25 @@
+import { Product } from './Product'
+import ProductListInterface from "./ProductListInterface.js";
+
+class Catalog implements ProductListInterface {
+    products: Product[] = []
+
+    async findById(id: number): Promise<Product> {
+    
+        let product: Product = this.products.find((p: Product) => p.id == id) as Product
+
+        await product.getComments()
+
+        return product
+
+    }
+
+    add(p: Product): Product {
+        this.products.push(p)
+        return p
+    }
+
+    showAll(): Product[]{
+        return this.products
+    }
+}
---
+++ ts/app/ProductListInterface.ts
@@ -0,0 +1,5 @@
+import { Product } from './Product'
+
+export default interface ProductListInterface {
+    products: Product[]
+}
