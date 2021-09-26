4e71c3c130d76cf2a1242d261d07e708178a52aa
Scarlet <smichelle05a@gmail.com>
2021-09-26 17:34:03 -0300

    add ts views files

---
+++ ts/Views/commentCard.ts
@@ -0,0 +1,14 @@
+import Product from "../app/Product.js";
+import { Comment } from "../app/Product.js";
+
+export default function commentCard(comment: Comment) {
+  return /* html */ `
+    <div class="card comment-card">
+        <div class="card-body">
+            <h5 class="card-comment-title">${comment.name}</h5>
+            <h6 class="card-subtitle">${comment.email}</h6>
+            <p class="card-text">${comment.body}</p>
+        </div>
+    </div>
+    `;
+}
---
+++ ts/Views/productCard.ts
@@ -0,0 +1,16 @@
+import Product from "../app/Product";
+
+export default function productCard(product: Product) {
+  return /* html */ `
+    <div class="col-card">
+        <div class="card">
+            <img src="img/${product.img}" alt="product image" class="card-img"/>
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <a href="/productDetail" class="btn nav-link js-product-detail" data-product-id="${product.id}">Go to product detail</a>
+                <button class="btn nav-link js-add-to-cart" data-product-id="${product.id}">Add to Cart</button>
+            </div>
+        </div>
+    </div>
+    `;
+}
