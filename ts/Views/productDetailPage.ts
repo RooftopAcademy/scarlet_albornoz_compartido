d2468ca06af978003d4b20a3c568bc6b156705b8
Scarlet <smichelle05a@gmail.com>
2021-10-13 16:05:35 -0300

    add buttons to product detail page

--- ts/Views/productDetailPage.ts
+++ ts/Views/productDetailPage.ts
@@ -10,6 +10,7 @@ export default function productDetailPage(product: Product): string {
     commentsHTML += commentCard(comment);
   });
 
+  let price = product.price.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })
   return /* html */ `
     <section class="section product-detail w-100 h-100 d-flex flex-col">
         <div class="col-card d-flex flex-col w-100 a-items-center">
@@ -17,11 +18,18 @@ export default function productDetailPage(product: Product): string {
                 <img src="img/${product.img}" alt="product image" class="card-img">
                 <div class="card-body">
                     <h5 class="card-title">${product.name}</h5>
-                    <h6 class="card-price">$${product.price}</h6>
+                    <h6 class="card-price">${price}</h6>
                     <p class="card-text">
                         ${product.description}
                     </p>
-                    <a href="/products" class="btn nav-link">Back to product list</a>
+                    <div class="d-flex">
+                        <span class="item-qty">
+                            <button class="btn btn-square js-remove" data-product-id="${product.id}">-</button>
+                            ${product.qty}
+                            <button class="btn btn-square js-add" data-product-id="${product.id}">+</button>
+                        </span>
+                        <a href="/products" class="btn nav-link">Back to product list</a>
+                    </div>
                 </div>
             </div>
         </div>
