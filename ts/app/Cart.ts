eb137055efbf479bdc4b2cbf8aad49542c6760d8
Scarlet <smichelle05a@gmail.com>
2021-10-13 16:04:40 -0300

    change cart functionalities

--- ts/app/Cart.ts
+++ ts/app/Cart.ts
@@ -3,49 +3,45 @@ import Catalog from "./Catalog";
 import { store } from "../app";
 
 export default class Cart extends Catalog {
-  findProduct(pId: number): number {
-    let product: number = this.products.findIndex((p: Product) => p.id == pId);
 
-    return product;
+  findInCart(pId: number): number {
+    let productIndex: number = this.products.findIndex((p: Product) => p.id == pId);
+
+    return productIndex;
   }
 
-  addToCart(pId: number): Product {
-    let index = this.findProduct(pId);
-    let product: Product;
+  override add(p: Product): void {
+
+    let product = p
+    let index = this.findInCart(product.id)
 
     if (index < 0) {
-      product = store.catalog.findById(pId);
-      product.qty += 1;
-      this.products.push(product);
+      product.qty += 1
+      this.products.push(product)
     } else {
-      product = this.products[index];
-      product.qty += 1;
+      product = this.products[index]
+      product.qty += 1
     }
-
-    return product;
   }
 
-  remove(pId: number): Product {
-    let index = this.findProduct(pId);
-    let product = this.products[index];
+  remove(p: Product): void {
+
+    let product = p
+    let index = this.findInCart(product.id)
 
     if (this.products[index].qty > 1) {
       product.qty -= 1;
     } else {
-      this.removeFromCart(index);
+      this.delete(product);
     }
-    return product;
   }
 
-  removeFromCart(pId: number): Product {
-    let index = this.findProduct(pId);
-    let product = this.products[index];
+  delete(p: Product): void {
 
+    let product = p
+    let index = this.findInCart(product.id)
+
+    product.qty = 0
     this.products.splice(index, 1);
-    return product;
-  }
-
-  clearCart(): void {
-    this.products = [];
   }
 }
