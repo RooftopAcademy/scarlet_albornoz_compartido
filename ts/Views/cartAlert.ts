e8f9b7a26b1875e749a34a08ef92d2d82f2b5c24
Scarlet <smichelle05a@gmail.com>
2021-10-14 16:33:26 -0300

    add close functionality to cart alert

--- ts/Views/cartAlert.ts
+++ ts/Views/cartAlert.ts
@@ -3,16 +3,16 @@ import Product from "../app/Product";
 export default function cartAlert(p: Product) {
     return /* html */ `
     <div class="alert d-flex a-items-center j-space-between show-alert">
-                <div class="d-flex a-items-center">
-                    <i class="fas fa-shopping-cart"></i>
-                    <img src="img/${p.img}" alt="${p.name}" class="alert-img">
-                </div>
-                <span class="alert-msg">
-                    ${p.name} <br /> added to cart
-                </span>
-                <button class="btn close-alert-btn d-flex a-items-center j-center">
-                    <span class="fas fa-times"></span>
-                </button>
-            </div>
+        <div class="d-flex a-items-center">
+            <i class="fas fa-shopping-cart"></i>
+            <img src="img/${p.img}" alt="${p.name}" class="alert-img">
+        </div>
+        <span class="alert-msg">
+            ${p.name} <br /> added to cart
+        </span>
+        <button class="btn close-alert-btn d-flex a-items-center j-center" onclick="this.parentNode.remove()">
+            <span class="fas fa-times"></span>
+        </button>
+    </div>
     `
 }
